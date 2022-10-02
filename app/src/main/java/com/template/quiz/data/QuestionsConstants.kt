c126e271f77600e7808033f3d907d977e1036d30
zafar-apk <abdullaevzafarchik@gmail.com>
2022-10-02 21:41:52 +0500

    added quiz questions particularly

--- app/src/main/java/com/template/quiz/data/QuestionsConstants.kt
+++ app/src/main/java/com/template/quiz/data/QuestionsConstants.kt
@@ -4,63 +4,92 @@ import com.template.quiz.domain.model.QuestionModel
 
 object QuestionsConstants {
     fun getQuestions() = mutableListOf(
-        QuestionModel("test.png",
-            "ЗАМЕНИТЬ_ВОПРОС_1",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+        QuestionModel(
+            null,
+            "The James Webb Space Telescope was launched on...",
+            listOf(
+                "25 December 2021",
+                "09 June 2022",
+                "15 January 2019",
+                "25 March 2020"
+            ),
+            "25 December 2021"
+        ),
 
-        QuestionModel(null,
-            "ЗАМЕНИТЬ_ВОПРОС_2",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+        QuestionModel(
+            null,
+            "What is the largest earth-like planet in the solar system?",
+            listOf(
+                "Earth",
+                "Mars, ",
+                "Mercury",
+                "Venus"
+            ),
+            "Earth"
+        ),
 
-        QuestionModel(null,
-            "ЗАМЕНИТЬ_ВОПРОС_3",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+        QuestionModel(
+            null,
+            "The original plan called for a 2007 launch date for the telescope, but various factors have delayed its completion and launch. Which of the following did not contribute to a delay?",
+            listOf(
+                "A CIA investigation into whether NASA scientists were Secret Communists.",
+                "The project running over budget.",
+                "Construction delays.",
+                "Components failing tests to see if they would function properly."
+            ),
+            "A CIA investigation into whether NASA scientists were Secret Communists."
+        ),
 
-        QuestionModel(null,
-            "ЗАМЕНИТЬ_ВОПРОС_4",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+        QuestionModel(
+            null,
+            "Where is the Lagrange point of the James Webb Telescope?",
+            listOf(
+                "Lagrange 2",
+                "Lagrange 1",
+                "Lagrange 3",
+                "Lagrange 4"
+            ),
+            "Lagrange 2"
+        ),
 
-        QuestionModel(null,
-            "ЗАМЕНИТЬ_ВОПРОС_5",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+        QuestionModel(
+            null,
+            "The JWST is planned to orbit the Sun at the Second Earth-Sun Lagrange Point. What is a Lagrange point?",
+            listOf(
+                "A point where the gravitational force between two objects balances out.",
+                "An astronomical measurement meaning 1/10th the distance between the Earth and the Sun",
+                "A point where a body can be between two objects and never be visible to the naked eye from either.",
+                "An astronomical measurement proposed by ZZ Top's Billy Gibbons."
+            ),
+            "A point where the gravitational force between two objects balances out."
+        ),
 
-        QuestionModel(null,
-            "ЗАМЕНИТЬ_ВОПРОС_6",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
+        QuestionModel(
+            null,
+            "How long after launch did The James Webb's  reach its planned orbital distance?",
+            listOf(
+                "ПРАВИЛЬНЫЙ_ОТВЕТ",
                 "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
                 "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"
+            ),
+            "ПРАВИЛЬНЫЙ_ОТВЕТ"
+        ),
 
-        QuestionModel(null,
+        QuestionModel(
+            null,
             "ЗАМЕНИТЬ_ВОПРОС_7",
-            listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
+            listOf(
+                "ПРАВИЛЬНЫЙ_ОТВЕТ",
                 "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
                 "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
-                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"),
-            "ПРАВИЛЬНЫЙ_ОТВЕТ"),
+                "НЕПРАВИЛЬНЫЙ_ОТВЕТ"
+            ),
+            "ПРАВИЛЬНЫЙ_ОТВЕТ"
+        ),
 
-        QuestionModel(null,
+        QuestionModel(
+            null,
             "ЗАМЕНИТЬ_ВОПРОС_8",
             listOf("ПРАВИЛЬНЫЙ_ОТВЕТ",
                 "НЕПРАВИЛЬНЫЙ_ОТВЕТ",
